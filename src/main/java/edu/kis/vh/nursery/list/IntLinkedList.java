c254a1793e1a548feff9341dc14a9405ebcb3566
Yurii Pokora <258650@edu.p.lodz.pl>
2026-04-13 03:22:28 +0200

    14. Przeniesienie Node do IntLinkedList jako prywatnej klasy wewnętrznej

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -38,4 +38,35 @@ public class IntLinkedList {
         return ret;
     }
 
+    private static class Node {
+
+        private int value;
+        private Node prev;
+        private Node next;
+
+        public Node(int i) {
+            value = i;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
 }
