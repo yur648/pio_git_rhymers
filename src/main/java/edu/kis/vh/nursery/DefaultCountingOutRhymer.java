6134b3f7b7540888952b6ae9eaf4f9a0a6a2d5d9
Yurii Pokora <258650@edu.p.lodz.pl>
2026-04-13 04:06:01 +0200

    17. Wygenerowanie i uzupełnienie dokumentacji JavaDoc dla DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,10 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa implementująca podstawowy mechanizm wyliczanki (stos).
+ * Pozwala na dodawanie elementów (countIn) i ich pobieranie (countOut).
+ * Działa na zasadzie LIFO (Last In, First Out).
+ */
 public class DefaultCountingOutRhymer {
 
     public static final int STACK_CAPACITY = 12;
@@ -9,29 +14,59 @@ public class DefaultCountingOutRhymer {
 
     private int total = EMPTY_STACK_INDICATOR ;
 
+    /**
+     * Zwraca aktualną wartość wskaźnika stosu.
+     *
+     * @return bieżąca wartość total
+     */
     public int getTotal() {
         return total;
     }
 
+    /**
+     * Dodaje element na szczyt stosu, jeśli stos nie jest pełny.
+     *
+     * @param in element do dodania
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     * Sprawdza, czy stos jest pusty.
+     *
+     * @return true jeśli stos jest pusty, false w przeciwnym razie
+     */
     public boolean callCheck() {
         return total == EMPTY_STACK_INDICATOR ;
     }
 
+    /**
+     * Sprawdza, czy stos jest pełny.
+     *
+     * @return true jeśli stos jest pełny, false w przeciwnym razie
+     */
     public boolean isFull() {
         return total == STACK_CAPACITY - 1;
     }
 
+    /**
+     * Podgląda element na szczycie stosu bez jego usuwania.
+     *
+     * @return wartość na szczycie stosu lub ERROR_RETURN_VALUE jeśli stos jest pusty
+     */
     protected int peekaboo() {
         if (callCheck())
             return ERROR_RETURN_VALUE;
         return numbers[total];
     }
 
+    /**
+     * Usuwa i zwraca element ze szczytu stosu.
+     *
+     * @return usunięty element lub ERROR_RETURN_VALUE jeśli stos jest pusty
+     */
     public int countOut() {
         if (callCheck())
             return ERROR_RETURN_VALUE;
